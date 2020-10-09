fbb9dd345ee1b97c482040823fc4f02fd2a6f402
marcoswebmasteroficial <48739175+marcoswebmasteroficial@users.noreply.github.com>
2020-10-09 10:40:51 -0300

    Update calculo.js

--- calculo.js
+++ calculo.js
@@ -1,13 +1,26 @@
-document.querySelector("button").addEventListener("click", () =>{
-var inicio = document.getElementById("origem").value;
-var fim = document.getElementById("destino").value;
-var request = {origin: inicio,destination: fim,travelMode: 'DRIVING'};
-var direcao = new google.maps.DirectionsService();
-direcao.route(request, function(result, status) {
-if (status == 'OK') {
-document.getElementById("resultado").textContent = result.routes[0].legs[0].distance.text;
-}else{
-alert('ERROR !! '+result)
+function initization() {
+    var direcao = new google.maps.DirectionsService();
+    document.querySelector("button").addEventListener("click", () => {
+    document.getElementById("resultado").style.display = 'none';
+    var inicio = document.getElementById("origem").value;
+    var fim = document.getElementById("destino").value;
+    var travel = document.getElementById("travel").value;
+    var frete = document.getElementById("km").value;
+    var mode;
+    switch (travel) {
+      case 'publico':  mode = 'TRANSIT'; break;
+      case 'carro':    mode = 'DRIVING'; break;
+      case 'ape':      mode = 'WALKING'; break;
+    }
+    var request = { origin: inicio,  destination: fim,  travelMode: mode  };
+    direcao.route(request, function(result, status) {
+      if (status == 'OK') {
+        document.getElementById("resultado").style.display = 'block';
+        document.getElementById("resultado").innerHTML = result.routes[0].legs[0].distance.text + ' <br> Valor  Estimando do Frete :' + (result.routes[0].legs[0].distance.text.split(' ')[0] * frete).toLocaleString('pt-br', {
+          style: 'currency',currency: 'BRL'});
+      } else {
+        alert('ERROR:'+result.status);
+      }
+    });
+  })
 }
-});
-})
